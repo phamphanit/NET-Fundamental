baf64690d89e07de05256c9f8b4f65a3cbcbf681
phamphan <phamphanthsp@gmail.com>
2021-07-10 14:15:16 +0700

    test 21

--- WebApplicationTest/Controllers/HomeController.cs
+++ WebApplicationTest/Controllers/HomeController.cs
@@ -32,7 +32,7 @@ namespace WebApplicationTest.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description pageeee.    kkkkk";
+            ViewData["Message"] = "Your application description pageeee.    kkkkk macccc";
 
             return View();
         }
