743d0c0732801f9123d28b3501758aecc5f193ab
phamphan <phamphanthsp@gmail.com>
2021-07-10 12:20:21 +0700

    test6

--- WebApplicationTest/Controllers/HomeController.cs
+++ WebApplicationTest/Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace WebApplicationTest.Controllers
         {
             var model = new IndexViewModel
             {
-                Players = _playerRepository.GetAll().ToList(),    // constructor injectedddddd
+                Players = _playerRepository.GetAll().ToList(),    // constructor injectedddddddddd
                 Games = gameRepository.GetTodaysGames().ToList()      // parameter injectedddd
             };
 
